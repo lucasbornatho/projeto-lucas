619574284129166510c3b76226512cbcd286a59c
Lucas Bornatho Pires <lucasbornatho@yahoo.com.br>
2025-04-14 22:22:31 -0300

    criando contexto e testando na home

--- src/App.tsx
+++ src/App.tsx
@@ -13,25 +13,28 @@ import RequisicoesTypesAsync from './pages/requisicoestypesasync'
 import RequisicoesPost from './pages/requisicoespost'
 import RequisicoesPostRefact from './pages/requisicoespostrefact'
 import RequisicoesParam from './pages/requisicoesparam'
+import { UsuarioLogadoProvider } from './contexts/contextAuth'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path='*' element= {<NotFound />} />
-      <Route path='/' element= {<Home />} />
-      <Route path='/sobre' element= {<Sobre />} />
-      <Route path='/cadastro' element= {<Cadastro />} />
-      <Route path='/lista' element= {<Lista />} />
-      <Route path='/condicional' element= {<Condicional />} />
-      <Route path='/comunicacao' element= {<Comunicacao />} />
-      <Route path='/requisicoes' element= {<Requisicoes />} />
-      <Route path='/requisicoestypes' element= {<RequisicoesTypes />} />
-      <Route path='/requisicoestypesasync' element= {<RequisicoesTypesAsync />} />
-      <Route path='/requisicoespost' element = {<RequisicoesPost />} />
-      <Route path='/requisicoespostrefact' element = {<RequisicoesPostRefact />} />
-      <Route path='/requisicoesparam' element = {<RequisicoesParam />} />
-    </Routes>
+    <UsuarioLogadoProvider>
+      <Routes>
+        <Route path='*' element= {<NotFound />} />
+        <Route path='/' element= {<Home />} />
+        <Route path='/sobre' element= {<Sobre />} />
+        <Route path='/cadastro' element= {<Cadastro />} />
+        <Route path='/lista' element= {<Lista />} />
+        <Route path='/condicional' element= {<Condicional />} />
+        <Route path='/comunicacao' element= {<Comunicacao />} />
+        <Route path='/requisicoes' element= {<Requisicoes />} />
+        <Route path='/requisicoestypes' element= {<RequisicoesTypes />} />
+        <Route path='/requisicoestypesasync' element= {<RequisicoesTypesAsync />} />
+        <Route path='/requisicoespost' element = {<RequisicoesPost />} />
+        <Route path='/requisicoespostrefact' element = {<RequisicoesPostRefact />} />
+        <Route path='/requisicoesparam' element = {<RequisicoesParam />} />
+      </Routes>
+    </UsuarioLogadoProvider>
   )
 }
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,23 @@
 import Rodape from "../components/rodape"
 import Cabecalho from "../components/cabecalho"
+import { useContext } from "react"
+import { UsuarioLogadoContext } from "../contexts/contextAuth"
 
 function Home () {
+
+    const UsuarioLogadoCtx = useContext(UsuarioLogadoContext);
+
+    function handleClick() {
+        UsuarioLogadoCtx?.setName('LUCASBPIRES');
+    }
+
     return (
         <div>
             <Cabecalho />
             <h1 className='home'>HOME!</h1>
+            <br />
+            <hr />
+            <button onClick={handleClick}>O usuário no contexto é:</button> {UsuarioLogadoCtx?.name}
             <Rodape />
         </div>
     )
